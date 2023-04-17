37f63b68b04c9e9a39e85a6dd551c3e41985b84e
Kevin Gutierrez <kevin.gutierrez@zeb.mx>
2023-04-16 22:56:24 -0600

    feat: update section subscribe

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -2,18 +2,24 @@ const Subscribe = () => {
   return (
     <section className="subscribe">
       <div className="container">
-        <div style={{backgroundImage: 'url(/images/subscribe.jpg)'}} className="subscribe__content">
-          <h4>Subscribe to our newsletter and receive exclusive offers every week</h4>
+        <div
+          style={{ backgroundImage: "url(/images/subscribe.jpg)" }}
+          className="subscribe__content"
+        >
+          <h4>
+            Suscríbete a nuestro boletín y recibe ofertas exclusivas cada semana
+          </h4>
 
           <form className="subscribe__form">
-            <input type="email" placeholder="Email address" />
-            <button type="submit" className="btn btn--rounded btn--yellow">Subscribe</button>
+            <input type="email" placeholder="Correo electrónico" />
+            <button type="submit" className="btn btn--rounded btn--yellow">
+              Suscribirse
+            </button>
           </form>
         </div>
       </div>
     </section>
-  )
+  );
 };
 
-
-export default Subscribe
+export default Subscribe;
