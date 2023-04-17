7724ee7636f71a15b726e1c17e7d313d7d32df2e
Kevin Gutierrez <kevin.gutierrez@zeb.mx>
2023-04-16 23:33:34 -0600

    feat: comment filters no use

--- components/products-filter/index.tsx
+++ components/products-filter/index.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import Checkbox from './form-builder/checkbox';
-import CheckboxColor from './form-builder/checkbox-color';
-import Slider from 'rc-slider';
+import { useState } from "react";
+// import Checkbox from "./form-builder/checkbox";
+// import CheckboxColor from "./form-builder/checkbox-color";
+import Slider from "rc-slider";
 
 // data
-import productsTypes from './../../utils/data/products-types';
-import productsColors from './../../utils/data/products-colors';
-import productsSizes from './../../utils/data/products-sizes';
+// import productsTypes from "./../../utils/data/products-types";
+// import productsColors from "./../../utils/data/products-colors";
+// import productsSizes from "./../../utils/data/products-sizes";
 
 const { createSliderWithTooltip } = Slider;
 const Range = createSliderWithTooltip(Slider.Range);
@@ -16,51 +16,61 @@ const ProductsFilter = () => {
 
   const addQueryParams = () => {
     // query params changes
-  }
+  };
 
   return (
     <form className="products-filter" onChange={addQueryParams}>
-      <button type="button" 
-        onClick={() => setFiltersOpen(!filtersOpen)} 
-        className={`products-filter__menu-btn ${filtersOpen ? 'products-filter__menu-btn--active' : ''}`}>
-          Add Filter <i className="icon-down-open"></i>
+      <button
+        type="button"
+        onClick={() => setFiltersOpen(!filtersOpen)}
+        className={`products-filter__menu-btn ${
+          filtersOpen ? "products-filter__menu-btn--active" : ""
+        }`}
+      >
+        Agregar Filtro <i className="icon-down-open"></i>
       </button>
-      
-      <div className={`products-filter__wrapper ${filtersOpen ? 'products-filter__wrapper--open' : ''}`}>
-        <div className="products-filter__block">
+
+      <div
+        className={`products-filter__wrapper ${
+          filtersOpen ? "products-filter__wrapper--open" : ""
+        }`}
+      >
+        {/* <div className="products-filter__block">
           <button type="button">Product type</button>
           <div className="products-filter__block__content">
-            {productsTypes.map(type => (
-              <Checkbox 
-                key={type.id} 
-                name="product-type" 
-                label={type.name} 
-              />
+            {productsTypes.map((type) => (
+              <Checkbox key={type.id} name="product-type" label={type.name} />
             ))}
           </div>
-        </div>
+        </div> */}
 
         <div className="products-filter__block">
-          <button type="button">Price</button>
+          <button type="button">Precio</button>
           <div className="products-filter__block__content">
-            <Range min={0} max={20} defaultValue={[3, 10]} tipFormatter={value => `${value}%`} />
+            <Range
+              min={0}
+              max={20}
+              defaultValue={[3, 10]}
+              tipFormatter={(value) => `${value}%`}
+            />
           </div>
         </div>
-        
+        {/* 
         <div className="products-filter__block">
           <button type="button">Size</button>
           <div className="products-filter__block__content checkbox-square-wrapper">
-            {productsSizes.map(type => (
-              <Checkbox 
-                type="square" 
-                key={type.id} 
-                name="product-size" 
-                label={type.label} />
+            {productsSizes.map((type) => (
+              <Checkbox
+                type="square"
+                key={type.id}
+                name="product-size"
+                label={type.label}
+              />
             ))}
           </div>
-        </div>
-        
-        <div className="products-filter__block">
+        </div> */}
+
+        {/* <div className="products-filter__block">
           <button type="button">Color</button>
           <div className="products-filter__block__content">
             <div className="checkbox-color-wrapper">
@@ -69,13 +79,17 @@ const ProductsFilter = () => {
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <button type="submit" className="btn btn-submit btn--rounded btn--yellow">Apply</button>
+        <button
+          type="submit"
+          className="btn btn-submit btn--rounded btn--yellow"
+        >
+          Aplicar
+        </button>
       </div>
     </form>
-  )
-}
-  
-export default ProductsFilter
-  
+  );
+};
+
+export default ProductsFilter;
