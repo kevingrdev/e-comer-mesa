8de96a1333f62ae9650c66499b3be341d71d6b3e
Kevin Gutierrez <kevin.gutierrez@zeb.mx>
2023-04-16 22:28:00 -0600

    feat: update page intro

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -15,31 +15,15 @@ const Header = ({ isErrorPage }: HeaderType) => {
   const { cartItems } = useSelector((state: RootState) => state.cart);
   const arrayPaths = ["/"];
 
-  const [onTop, setOnTop] = useState(
-    !arrayPaths.includes(router.pathname) || isErrorPage ? false : true
-  );
   const [menuOpen, setMenuOpen] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
   const navRef = useRef(null);
   const searchRef = useRef(null);
 
-  const headerClass = () => {
-    if (window.pageYOffset === 0) {
-      setOnTop(true);
-    } else {
-      setOnTop(false);
-    }
-  };
-
   useEffect(() => {
     if (!arrayPaths.includes(router.pathname) || isErrorPage) {
       return;
     }
-
-    headerClass();
-    window.onscroll = function () {
-      headerClass();
-    };
   }, []);
 
   const closeMenu = () => {
@@ -55,7 +39,7 @@ const Header = ({ isErrorPage }: HeaderType) => {
   useOnClickOutside(searchRef, closeSearch);
 
   return (
-    <header className={`site-header ${!onTop ? "site-header--fixed" : ""}`}>
+    <header className={`site-header site-header--fixed `}>
       <div className="container">
         <Link href="/">
           <a>
--- components/page-intro/index.tsx
+++ components/page-intro/index.tsx
@@ -1,30 +1,26 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {EffectFade, Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { EffectFade, Navigation } from "swiper";
+import Link from "next/link";
 
 SwiperCore.use([EffectFade, Navigation]);
 
 const PageIntro = () => {
-
   return (
-    <section className="page-intro">  
-      <Swiper navigation effect="fade" className="swiper-wrapper">
+    <section className="page-intro">
+      <Swiper effect="fade" className="swiper-wrapper">
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-1.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
-                <h2>Sale of the summer collection</h2>
-                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-2.jpg')" }}>
-            <div className="container">
-              <div className="page-intro__slide__content">
-                <h2>Make your house into a home</h2>
-                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+                <h2>Todo lo que necesitas para tus proyectos</h2>
+                <Link href="/products">
+                  <a href="#" className="btn-shop">
+                    <i className="icon-right"></i>Ver productos
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -37,31 +33,34 @@ const PageIntro = () => {
             <li>
               <i className="icon-shipping"></i>
               <div className="data-item__content">
-                <h4>Free Shipping</h4>
-                <p>On purchases over $199</p>
+                <h4>Envío gratis</h4>
+                <p>En compras superiores a $199</p>
               </div>
             </li>
-            
+
             <li>
               <i className="icon-shipping"></i>
               <div className="data-item__content">
-                <h4>99% Satisfied Customers</h4>
-                <p>Our clients' opinions speak for themselves</p>
+                <h4>Clientes satisfechos al 99%</h4>
+                <p>Las opiniones de nuestros clientes hablan por sí mismas</p>
               </div>
             </li>
-            
+
             <li>
               <i className="icon-cash"></i>
               <div className="data-item__content">
-                <h4>Originality Guaranteed</h4>
-                <p>30 days warranty for each product from our store</p>
+                <h4>Garantía de originalidad</h4>
+                <p>
+                  Ofrecemos una garantía de 30 días para cada herramienta que
+                  vendemos
+                </p>
               </div>
             </li>
           </ul>
         </div>
       </div>
     </section>
-  )
+  );
 };
 
-export default PageIntro
+export default PageIntro;
