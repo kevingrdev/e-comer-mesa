272b3fee1691533fb5e179173898ce4e62b5617e
Kevin Gutierrez <kevin.gutierrez@zeb.mx>
2023-04-16 23:44:05 -0600

    feat: update products  page

--- components/products-content/index.tsx
+++ components/products-content/index.tsx
@@ -1,28 +1,32 @@
-import { useState } from 'react';
-import List from './list';
+import { useState } from "react";
+import List from "./list";
 
 const ProductsContent = () => {
   const [orderProductsOpen, setOrderProductsOpen] = useState(false);
-  
+
   return (
     <section className="products-content">
       <div className="products-content__intro">
-        <h2>Men's Tops <span>(133)</span></h2>
-        <button type="button" onClick={() => setOrderProductsOpen(!orderProductsOpen)} className="products-filter-btn"><i className="icon-filters"></i></button>
-        <form className={`products-content__filter ${orderProductsOpen ? 'products-order-open' : ''}`}>
+        <h2>
+          Herramientas eléctricas <span>(210)</span>
+        </h2>
+        <button
+          type="button"
+          onClick={() => setOrderProductsOpen(!orderProductsOpen)}
+          className="products-filter-btn"
+        >
+          <i className="icon-filters"></i>
+        </button>
+        <form
+          className={`products-content__filter ${
+            orderProductsOpen ? "products-order-open" : ""
+          }`}
+        >
           <div className="products__filter__select">
-            <h4>Show products: </h4>
+            <h4>Ordenar por: </h4>
             <div className="select-wrapper">
               <select>
-                <option>Popular</option>
-              </select>
-            </div>
-          </div>
-          <div className="products__filter__select">
-            <h4>Sort by: </h4>
-            <div className="select-wrapper">
-              <select>
-                <option>Popular</option>
+                <option>Populares</option>
               </select>
             </div>
           </div>
@@ -33,6 +37,5 @@ const ProductsContent = () => {
     </section>
   );
 };
-  
-export default ProductsContent
-  
+
+export default ProductsContent;
